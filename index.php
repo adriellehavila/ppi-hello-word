49c598f3b40e45f673ecef2a825f25ac307d0d0d
Adrielle Hávila Alves de Lima <adrielle.alves@academico.ifrn.edu.br>
2022-01-04 08:25:56 -0300

    Conectar banco do Heroku

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello Word no Git        
+    <title>Hello Word no Git    
     </title>
 </head>
 <body>
- <h1> Adrielle Havila </h1>   
+ <h1>Adrielle Havila</h1>   
 </body>
 </html>
